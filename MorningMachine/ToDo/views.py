949db06ceadb1bdc3934934e0de414fd7d0d0e32
aag2113 <gall.adam@gmail.com>
2015-03-10 13:47:30 -0400

    Tasks to AJAX

--- MorningMachine/ToDo/views.py
+++ MorningMachine/ToDo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.core.urlresolvers import reverse
 from django.views import generic
 from django.utils import timezone
@@ -32,7 +32,7 @@ def check(request, task_id):
 		else:
 			p.status = 0
 		p.save()
-		return HttpResponseRedirect(reverse('ToDo:index'))
+		return HttpResponse("success")
 	except (KeyError, Task.DoesNotExist):
 		return render(request, 'ToDo/task.html', {'task': p, 'error_message': "No Such task"})
 
@@ -46,7 +46,7 @@ def createTask(request):
 	qd = request.POST
 	p = TaskList.objects.get(pk=qd.get('taskList'))
 	task = Task.objects.create(taskList=p, title=qd.get('title'))
-	response = HttpResponse(generateTaskForm(task))
+	response = JsonResponse({'taskid':task.id,'msg':generateTask(task)})
 	return response
 
 def saveWidgetSize(request, tasklist_id):
@@ -60,8 +60,14 @@ def saveWidgetSize(request, tasklist_id):
 def saveWidgetPos(request, tasklist_id):
 	p = get_object_or_404(TaskList, pk=tasklist_id)
 	qd = request.POST
-	p.top = qd.get('t')
-	p.left = qd.get('l')
+	if qd.get('t') > 0:
+		p.top = qd.get('t')
+	else:
+		p.top = 0
+	if qd.get('l') > 0:
+		p.left = qd.get('l')
+	else:
+		p.left = 0
 	p.save()
 	return HttpResponseRedirect(reverse('ToDo:index'))
 
@@ -73,19 +79,20 @@ def clearCompleted(request, tasklist_id):
 			t.save()
 	return HttpResponse(renderTaskList(tasklist_id))
 
-def generateTaskForm(task):
-	result = '<form action="/ToDo/task/'+repr(task.id)+'/check/" method="post">'
-	result += '<input type="checkbox" class="task" name="task" value="'+repr(task.id)
+def generateTask(task):
+	result = '<div class="task" data-taskid="'+repr(task.id)+'">'
+	result += '<input type="checkbox"'
 	if task.status == 1:
-		result += 'checked'
-	result += '><div class="taskTitle" title="'+repr(task.id)+'"> '+task.title+'</div></input></form>'
+		result += ' checked'
+	result += '></input>'
+	result += '<div class="taskTitle">'+task.title+'</div></div>'
 	return result
 
 def renderTaskList(tasklist_id):
 	tl = TaskList.objects.get(pk=tasklist_id)
-	tlHTML = '<div id="tasks" data-tasklistid='+repr(tasklist_id)+'>'
+	tlHTML = '<div class="tasks" data-tasklistid="'+repr(int(tasklist_id))+'">'
 	for t in tl.task_set.all():
 		if t.status >= 0:
-			tlHTML += generateTaskForm(t)
+			tlHTML += generateTask(t)
 	tlHTML += '</div>'
 	return tlHTML	
